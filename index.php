b57f4067b7d6d948a7a13f4bc96f6b86baf1a1fe
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-22 17:47:23 +0200

    struttura grafica base

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if ($caratteriScelti && $_GET['caratteriScelti']) {
 
 function generaPassword($opzioneScelta,$lunghezzaPassword,$ripetizione) {
     $lunghezzaMax = strlen($opzioneScelta) - 1;
-    if ($lunghezzaPassword > $lunghezzaMax){
+    if ($lunghezzaPassword > $lunghezzaMax ){
         $lunghezzaPassword = $lunghezzaMax;
     }
     $password = '';
@@ -84,43 +84,67 @@ function generaPassword($opzioneScelta,$lunghezzaPassword,$ripetizione) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
     <title>Document</title>
 </head>
-<body>
+<body class="container">
+    <div class="text-center col-12">
+        <h1 class=>
+            Strong Password Generator
+        </h1>
+        <h2 style="color : white">
+            Genera una password sicura
+        </h2>
+    </div>
 
-<legend>Cosa vuoi includere?</legend>
-<form method="GET" action="index.php">
-    <div>
-        <label for="coding">Lettere ripetetute</label>
-        <input type="radio" id="coding" name="lettereRepeat" value="0" /><span>si</span>
-        <input type="radio" id="coding" name="lettereRepeat" value="1" /><span>no</span>
+    <div class="col-12 container2 row">
+        <form method="GET" action="index.php">
+            <div class="col-12 row">
+                <div class="col-6">
+                    <span>
+                        Lunghezza caratteri password
+                    </span>
+                </div>
+                <div class="col-6">
+                    <input type="number" name='lunghezzaPassword' min="1" required>  
+                </div>
+            </div>
+            <div class="col-12 row">
+                <div class="col-6">
+                    <label for="coding">Lettere ripetetute</label>
+                </div>
+                <div class="col-6 row flex-column mt-2">
+                    <div>
+                        <input type="radio" id="coding" name="lettereRepeat" value="0" required/><span>si</span>
+                    </div>
+                    <div>
+                        <input type="radio" id="coding" name="lettereRepeat" value="1" /><span>no</span>
+                    </div>
+                    <div class="mt-3">
+                      <input type="checkbox" id="coding" name="caratteriScelti[]" value="1" />
+                      <label for="coding">Lettere Minuscole</label>
+                    </div>
+                    <div>
+                      <input type="checkbox" id="music" name="caratteriScelti[]" value="2" />
+                      <label for="music">Lettere Maiuscole</label>
+                    </div>
+                    <div>
+                      <input type="checkbox" id="music" name="caratteriScelti[]" value="3" />
+                      <label for="music">Numeri</label>
+                    </div>
+                    <div>
+                      <input type="checkbox" id="music" name="caratteriScelti[]" value="4" />
+                      <label for="music">Caratteri speciali</label>
+                    </div>
+                </div>
+            </div>
+              
+            <button type="button" class="btn btn-primary mt-3">Vai!</button>
+            <button type="reset" class="btn btn-secondary mt-3">Annulla</button> 
+        </form>
+
     </div>
-    <div>
-      <input type="checkbox" id="coding" name="caratteriScelti[]" value="1" />
-      <label for="coding">Lettere Minuscole</label>
-    </div>
-    <div>
-      <input type="checkbox" id="music" name="caratteriScelti[]" value="2" />
-      <label for="music">Lettere Maiuscole</label>
-    </div>
-    <div>
-      <input type="checkbox" id="music" name="caratteriScelti[]" value="3" />
-      <label for="music">Numeri</label>
-    </div>
-    <div>
-      <input type="checkbox" id="music" name="caratteriScelti[]" value="4" />
-      <label for="music">Caratteri speciali</label>
-    </div>
-    <input type="number" name='lunghezzaPassword'>    
-    <button> vai!</button>    
-    <p>
-        <?php if ($opzioneScelta !== '') {
-            $passwordFinale = generaPassword($opzioneScelta,$lunghezzaPassword,$ripetizione);
-            echo "LA TUA PASSWORD E' : " . $passwordFinale;
-        }
-        ?>
-    </p>
-</form>
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </body>
 </html>
